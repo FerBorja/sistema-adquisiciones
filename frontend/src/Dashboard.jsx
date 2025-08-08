423b8f1257d00b5988a361bb8d90059a8631b0f2
Luis Fernando Casas Borja <luisfernandocasasborja@gmail.com>
2025-08-08 08:35:02 -0600

    Removing of frontend files

--- frontend/src/Dashboard.jsx
+++
@@ -1,8 +0,0 @@
-export default function Dashboard() {
-  return (
-    <div className="p-6">
-      <h2 className="text-2xl font-bold">Bienvenido al Dashboard</h2>
-      <p>Aquí irá la pantalla principal después de login.</p>
-    </div>
-  )
-}
